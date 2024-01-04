9f0d1f50c553a007f450c2ca69f96bb76113cb53
Marc Ströbel <m.stroebel@gmx.de>
2024-01-04 19:50:43 +0100

    fix

--- src/Application/Actions/Login/LogoutAction.php
+++ src/Application/Actions/Login/LogoutAction.php
@@ -5,25 +5,15 @@ declare(strict_types=1);
 namespace App\Application\Actions\Login;
 
 use App\Application\Actions\Action;
-use League\OAuth2\Server\Repositories\UserRepositoryInterface;
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Log\LoggerInterface;
-use Slim\Views\Twig;
 
 class LogoutAction extends Action
 {
-    private Twig $twig;
-    private UserRepositoryInterface $userRepo;
-
     public function __construct(
-        LoggerInterface $logger,
-        ContainerInterface $container,
-        UserRepositoryInterface $userRepo
+        LoggerInterface $logger
     ) {
         parent::__construct($logger);
-        $this->twig = $container->get('view');
-        $this->userRepo = $userRepo;
     }
 
     /**
@@ -33,6 +23,6 @@ class LogoutAction extends Action
     {
         $_SESSION = [];
 
-        return $this->twig->render($this->response, 'index.html', ['is_authenticated' => false]);
+        return $this->response->withHeader('Location', '/');
     }
 }
