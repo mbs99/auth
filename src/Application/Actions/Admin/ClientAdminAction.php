5f113bafd6db2310cd41ce06969b9ed4722eb2db
Marc Ströbel <m.stroebel@gmx.de>
2024-01-19 15:55:39 +0100

    fix

--- src/Application/Actions/Admin/ClientAdminAction.php
+++ src/Application/Actions/Admin/ClientAdminAction.php
@@ -38,13 +38,13 @@ class ClientAdminAction extends Action
                 $queryParams = $this->request->getQueryParams();
                 $this->logger->debug('query = ' . print_r($queryParams, true), [$this::class]);
 
-                if (is_array($queryParams && 'true' == $queryParams['edit'])) {
+                if (is_array($queryParams) && 'true' == $queryParams['edit']) {
                     $id = $this->resolveArg('id');
                     $client = $this->clientAdminRepositoryInterface->getClientEntity($id);
                     $this->logger->debug('client = ' . print_r($client, true), [$this::class]);
                     $this->logger->debug('edit = true');
                     return $this->twig->render($this->response, 'admin_clients_edit.html', ['client' => $client, 'edit' => true]);
-                } else if (is_array($queryParams && 'false' == $queryParams['edit'])) {
+                } else if (is_array($queryParams) && 'false' == $queryParams['edit']) {
                     $id = $this->resolveArg('id');
                     $client = $this->clientAdminRepositoryInterface->getClientEntity($id);
                     $this->logger->debug('client = ' . print_r($client, true), [$this::class]);
